50c175e2ea7d7fec0ca71bb2951e535bdd0e0ca1
WahyuPermana <triwahyup003@gmail.com>
2020-10-07 06:21:27 +0700

    Update Rekam_MedisController.php

--- rm/Controllers/Rekam_MedisController.php
+++ rm/Controllers/Rekam_MedisController.php
@@ -27,7 +27,7 @@ class Rekam_MedisController extends Controller
 	}
 	
 	// method untuk insert data ke table
-	public function storeRekan_Medis(Request $request)
+	public function storeRekam_Medis(Request $request)
 {
 	// insert data ke table
 	DB::table('rekam_medis')->insert([
@@ -47,21 +47,35 @@ class Rekam_MedisController extends Controller
 	public function editRekam_Medis($id_rekam_medis)
 {
 	$rekam_medis = DB::table('rekam_medis')->where('id_rekam_medis',$id_rekam_medis)->get();
+	$pasien = DB::table('pasien')->get();
+	$petugas_admin = DB::table('petugas_admin')->get();
+	$dokter = DB::table('dokter')->get();
 	// passing data rekam_medis yang didapat ke view edit.blade.php
-	return view('editRekam_Medis',['rekam_medis' => $rekam_medis]);
+	return view('editRekam_Medis',['rekam_medis' => $rekam_medis,'pasien' => $pasien,'petugas_admin' => $petugas_admin,'dokter' => $dokter]);
 }
 	public function updateRekam_Medis(Request $request)
 {
 	// update data
-	DB::table('dokter')->where('id_dokter',$request->id_dokter)->update([
+	DB::table('rekam_medis')->where('id_rekam_medis',$request->id_rekam_medis)->update([
+		'id_rekam_medis' => $request->id_rekam_medis,
+		'id_pasien' => $request->id_pasien,
+		'id_petugas' => $request->id_petugas,
 		'id_dokter' => $request->id_dokter,
-		'nama_dokter' => $request->nama_dokter,
-		'alamat' => $request->alamat,
-		'kota_tinggal' => $request->kota_tinggal,
-		'no_tlp' => $request->no_tlp,
-		'spesialis' => $request->spesialis
+		'tgl_periksa' => $request->tgl_periksa,
+		'diagnosa' => $request->diagnosa,
+		'keterangan' => $request->keterangan,
+		'biaya_jasa' => $request->biaya_jasa
 	]);
-	return redirect('/dokter');
+	return redirect('/rekam_medis');
+	}
+	// method untuk hapus data rekam medis
+	public function hapus($id_rekam_medis)
+	{
+		// menghapus data rekam medis berdasarkan id yang dipilih
+		DB::table('rekam_medis')->where('id_rekam_medis',$id_rekam_medis)->delete();
+		
+		// alihkan halaman ke halaman rekam medis
+		return redirect('/rekam_medis');
 	}
 }
 
