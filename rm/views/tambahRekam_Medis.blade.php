62e276727f831897daf9a9e70b8b1b1e458d72c9
WahyuPermana <triwahyup003@gmail.com>
2020-10-07 07:21:26 +0700

    Update tambahRekam_Medis.blade.php

--- rm/views/tambahRekam_Medis.blade.php
+++ rm/views/tambahRekam_Medis.blade.php
@@ -16,9 +16,9 @@
 	<form action="/rekam_medis/store" method="post">
 		{{ csrf_field() }}
 		Id Rekam Medis <input type="text" name="id_rekam_medis" required="required"> <br/>
-        Id Pasien <select name="id_pasien" required="required">@foreach ($pasien as $p)<option value="{{ $p -> id_pasien }}">{{$p -> nama_pasien}}</option>@endforeach</select></br>
-        Id Petugas <select name="id_petugas" required="required">@foreach ($petugas_admin as $pa)<option value="{{ $pa -> id_petugas }}">{{$pa -> nama_petugas}}</option>@endforeach</select></br>
-        Id Dokter <select name="id_dokter" required="required">@foreach ($dokter as $d)<option value="{{ $d -> id_dokter }}">{{$d -> nama_dokter}}</option>@endforeach</select></br>
+        Nama Pasien <select name="id_pasien" required="required">@foreach ($pasien as $p)<option value="{{ $p -> id_pasien }}">{{$p -> nama_pasien}}</option>@endforeach</select></br>
+        Nama Petugas <select name="id_petugas" required="required">@foreach ($petugas_admin as $pa)<option value="{{ $pa -> id_petugas }}">{{$pa -> nama_petugas}}</option>@endforeach</select></br>
+        Nama Dokter <select name="id_dokter" required="required">@foreach ($dokter as $d)<option value="{{ $d -> id_dokter }}">{{$d -> nama_dokter}}</option>@endforeach</select></br>
 		Tgl Periksa <input type="text" name="tgl_periksa" required="required"> <br/>
 		Diagnosa <input type="text" name="diagnosa" required="required"> <br/>
 		Keterangan <input type="text" name="keterangan" required="required"> <br/>
