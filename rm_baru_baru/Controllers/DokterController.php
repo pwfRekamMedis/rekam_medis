7ea2c9c8ebec1d41869de1b0633b453b76820d28
WahyuPermana <triwahyup003@gmail.com>
2020-10-25 07:21:01 +0700

    Update DokterController.php

--- rm_baru_baru/Controllers/DokterController.php
+++ rm_baru_baru/Controllers/DokterController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\dokter;
 
 class DokterController extends Controller
 {
 	public function indexDokter()
     {
+		/*
     	// mengambil data dari table dokter
     	$dokter = DB::table('dokter')->get();
  
     	// mengirim data dokter ke view index
-    	return view('index/indexDokter',['dokter' => $dokter]);
+		return view('index/indexDokter',['dokter' => $dokter]);
+		*/
+		
+		// mengambil data dokter
+    	$dokter = dokter::all();
+ 
+    	// mengirim data dokter ke view dokter
+		return view('index/indexDokter',['dokter' => $dokter]);
+		
  
     }
     //
@@ -28,7 +38,8 @@ class DokterController extends Controller
 	public function storeDokter(Request $request)
 {
 	// insert data ke table
-	DB::table('dokter')->insert([
+	//DB::table('dokter')->insert([
+	dokter::create([
 		//'id_dokter' => $request->id_dokter,
 		'nama_dokter' => $request->nama_dokter,
 		'alamat' => $request->alamat,
@@ -36,19 +47,22 @@ class DokterController extends Controller
 		'no_tlp' => $request->no_tlp,
 		'spesialis' => $request->spesialis
 	]);
+
 	// alihkan halaman ke halaman
 	return redirect('/dokter');
  
 }
 	public function editDokter($id_dokter)
 {
-	$dokter = DB::table('dokter')->where('id_dokter',$id_dokter)->get();
+	//$dokter = DB::table('dokter')->where('id_dokter',$id_dokter)->get();
+	$dokter = dokter::find($id_dokter);
 	// passing data pegawai yang didapat ke view edit.blade.php
 	return view('edit/editDokter',['dokter' => $dokter]);
 }
 	public function updateDokter(Request $request)
 {
 	// update data
+	/*
 	DB::table('dokter')->where('id_dokter',$request->id_dokter)->update([
 		//'id_dokter' => $request->id_dokter,
 		'nama_dokter' => $request->nama_dokter,
@@ -57,6 +71,23 @@ class DokterController extends Controller
 		'no_tlp' => $request->no_tlp,
 		'spesialis' => $request->spesialis
 	]);
+	*/
+	$this->validate($request,[
+		//'id_dokter' => 'required',
+		'nama_dokter' => 'required',
+		'alamat' => 'required',
+		'kota_tinggal' => 'required',
+		'no_tlp' => 'required',
+		'spesialis' => 'required'
+	 ]);
+	$dokter = dokter::find($request->id_dokter);
+    $dokter->nama_dokter = $request->nama_dokter;
+	$dokter->alamat = $request->alamat;
+	$dokter->kota_tinggal = $request->kota_tinggal;
+	$dokter->no_tlp = $request->no_tlp;
+	$dokter->spesialis = $request->spesialis;
+	$dokter->save();
+	
 	return redirect('/dokter');
 }
 	// method untuk hapus data dokter
