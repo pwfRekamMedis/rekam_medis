2dc13df94129d48005f11dca2d96ae16d760906f
WahyuPermana <triwahyup003@gmail.com>
2020-10-25 07:21:13 +0700

    Update Petugas_AdminController.php

--- rm_baru_baru/Controllers/Petugas_AdminController.php
+++ rm_baru_baru/Controllers/Petugas_AdminController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\petugas_admin;
 
 class Petugas_AdminController extends Controller
 {
 	public function indexPetugas_Admin()
     {
-    	// mengambil data dari table dokter
+		/*
+    	// mengambil data dari table petugas admin
     	$petugas_admin = DB::table('petugas_admin')->get();
  
-    	// mengirim data dokter ke view index
-    	return view('index/indexPetugas_Admin',['petugas_admin' => $petugas_admin]);
+    	// mengirim data petugas admin ke view index
+		return view('index/indexPetugas_Admin',['petugas_admin' => $petugas_admin]);
+		*/
+		// mengambil data petugas admin
+    	$petugas_admin = petugas_admin::all();
+ 
+    	// mengirim data ke view petugas
+		return view('index/indexPetugas_Admin',['petugas_admin' => $petugas_admin]);
  
     }
     //
@@ -28,7 +36,8 @@ class Petugas_AdminController extends Controller
 	public function storePetugas_Admin(Request $request)
 {
 	// insert data ke table
-	DB::table('petugas_admin')->insert([
+	//DB::table('petugas_admin')->insert([
+		petugas_admin::create([
 		//'id_petugas' => $request->id_petugas,
 		'nama_petugas' => $request->nama_petugas,
 		'alamat' => $request->alamat,
@@ -41,13 +50,15 @@ class Petugas_AdminController extends Controller
 }
 	public function editPetugas_Admin($id_petugas)
 {
-	$petugas_admin = DB::table('petugas_admin')->where('id_petugas',$id_petugas)->get();
+	//$petugas_admin = DB::table('petugas_admin')->where('id_petugas',$id_petugas)->get();
+	$petugas_admin = petugas_admin::find($id_petugas);
 	// passing data pegawai yang didapat ke view edit.blade.php
 	return view('edit/editPetugas_Admin',['petugas_admin' => $petugas_admin]);
 }
 	public function updatePetugas_Admin(Request $request)
 {
 	// update data
+	/*
 	DB::table('petugas_admin')->where('id_petugas',$request->id_petugas)->update([
 		//'id_petugas' => $request->id_petugas,
 		'nama_petugas' => $request->nama_petugas,
@@ -55,6 +66,19 @@ class Petugas_AdminController extends Controller
 		'kota_tinggal' => $request->kota_tinggal,
 		'no_tlp' => $request->no_tlp
 	]);
+	*/
+	$this->validate($request,[
+		'nama_petugas' => 'required',
+		'alamat' => 'required',
+		'kota_tinggal' => 'required',
+		'no_tlp' => 'required'
+	 ]);
+	$petugas_admin = petugas_admin::find($request->id_petugas);
+    $petugas_admin->nama_petugas = $request->nama_petugas;
+	$petugas_admin->alamat = $request->alamat;
+	$petugas_admin->kota_tinggal = $request->kota_tinggal;
+	$petugas_admin->no_tlp = $request->no_tlp;
+	$petugas_admin->save();
 	return redirect('/petugas_admin');
 }
 	// method untuk hapus data petugas admin
