ad1330df5612d3539cc6f2758c7db44895c39690
WahyuPermana <triwahyup003@gmail.com>
2020-10-26 09:44:11 +0700

    Update obat.php

--- rm_baru_baru/obat.php
+++ rm_baru_baru/obat.php
@@ -9,4 +9,8 @@ class obat extends Model
     protected $table = "obat";
     protected $primaryKey =  "id_obat";
     protected $fillable = ['id_obat','kategori_obat','nama_obat','bentuk_obat','tgl_kadaluarsa','harga_obat'];
+
+    public function resep_obat(){
+        return $this->hasMany(resep_obat::class,'id_obat');
+    }
 }
