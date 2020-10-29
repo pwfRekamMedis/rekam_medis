2b7f5473a270392d51ca2b80a1846ebaf1eb9503
WahyuPermana <triwahyup003@gmail.com>
2020-10-29 20:04:21 +0700

    Update indexPetugas_Admin.blade.php

--- rm_baru_2/resources/views/index/indexPetugas_Admin.blade.php
+++ rm_baru_2/resources/views/index/indexPetugas_Admin.blade.php
@@ -36,6 +36,13 @@
 						<div>
 							<a href="/petugas_admin/tambah" button type="button" class="btn btn-success btn-sm"> 
 							<i class="fa fa-plus-square"></i>&nbsp; Tambah Data Petugas</button></a>
+
+							<div class="search-container">
+                            <form method="GET" action="/petugas_admin">
+                                <input type="text" name="cari" placeholder="Search ..." name="Search">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
+
 						</div>
                         <div class="card-body">
                   <table id="bootstrap-data-table" class="table table-striped table-bordered">
